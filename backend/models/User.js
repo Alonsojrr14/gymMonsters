9e4bc1efb7297eca0a6698ee78e6ed49b2df0ba4
Alonso Almeida <108317391+Alonsojrr14@users.noreply.github.com>
2025-05-14 20:49:47 -0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const WorkoutSchema = new mongoose.Schema({
+  name: { type: String, required: false },
   duration: Number,
   calories: Number,
   photo: String,
